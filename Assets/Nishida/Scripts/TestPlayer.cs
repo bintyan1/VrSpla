0b760ca84ab15e7c3b74694bf2da9c62f12489d6
Nimara0621 <53933126+Nimara0621@users.noreply.github.com>
2023-01-24 19:01:19 +0900

    スライム自動追尾

--- Assets/Nishida/Scripts/TestPlayer.cs
+++ Assets/Nishida/Scripts/TestPlayer.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeMove : MonoBehaviour
+public class TestPlayer : MonoBehaviour
 {
  //Rigidbody型のrbという変数を作る
     private Rigidbody rb;
-    public Transform target;
-
-    public float damping;
 
     public float power;
 
@@ -21,15 +18,9 @@ public class SlimeMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var lookPos = target.position - transform.position;
-lookPos.y = 0;
-var rotation = Quaternion.LookRotation(lookPos);
-transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
          //キー入力による値を代入(解説は後で
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
-
-        //↑で代入された値をVector3型のIdouHoukouという変数に代入する
         Vector3 force = new Vector3(x, 0, z);
 
         //rbに力を加える
